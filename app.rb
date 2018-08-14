53afeb5a535a93c74a9263513e60efbff2498433
Taylor Otey <taylorotey5@gmail.com>
2018-08-14 19:07:25 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+  get '/name' do
+    "My name is Taylor"
+  end
+
+  get '/hometown' do
+    "My hometown is Chicago"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Hamilton"
+  end
 end
+
+
+
+
