b4b98a0cb27788f0a464ee0b7d490ec31ad61d73
Taylor Otey <taylorotey5@gmail.com>
2018-08-14 19:07:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,18 +4,6 @@ class App < Sinatra::Base
 end
 
 
-  get '/name' do
-    "My name is Taylor"
-  end
-
-  get '/hometown' do
-    "My hometown is Chicago"
-  end
-
-  get '/favorite-song' do
-    "My favorite song is Hamilton"
-  end
-end
 
 
 
