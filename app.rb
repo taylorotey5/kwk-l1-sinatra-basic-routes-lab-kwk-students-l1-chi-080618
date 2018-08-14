cd9cd9473b4c54ac75652ec18482686caf7d577c
Taylor Otey <taylorotey5@gmail.com>
2018-08-14 19:07:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,9 +1,20 @@
 require_relative 'config/environment'
 
-class App < Sinatra::Base
+class App < Sinatra::Bas
+
+
+  get '/name' do
+    "My name is Taylor"
+  end
+
+  get '/hometown' do
+    "My hometown is Chicago"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Hamilton"
+  end
 end
-
-
 
 
 
